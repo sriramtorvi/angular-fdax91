eb524da24226c0dad7ed9cde03a56767356f4b06
sriramtorvi <sriram.torvi@gmail.com>
2020-01-05 19:20:22 +0530

    Removed components for new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ServerComponent ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
