e0936255bf0bfafcf6f4e1b864d7b134d80c7ba7
sriramtorvi <sriram.torvi@gmail.com>
2020-01-09 15:57:18 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  
+  selectedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.selectedFeature = feature;
+  }
 }
