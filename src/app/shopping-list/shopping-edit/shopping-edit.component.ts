50504fc6a67f8941eabff941d503518a5a94e6a5
sriramtorvi <sriram.torvi@gmail.com>
2020-01-09 20:47:26 +0530

    2 files modified

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @ViewChild('nameInput',{static: true}) nameInput
+  @ViewChild('amountInput',{static: true}) amountInput
+  onIngredientAdded
   constructor() { }
 
   ngOnInit() {
   }
 
+  onClickAdd(){
+
+  }
 }
